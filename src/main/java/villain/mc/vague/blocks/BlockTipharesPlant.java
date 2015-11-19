d9d2acf31f7ad6665a3d73c13b019829baa7ab7f
BlisterFingers <blister0fingers@gmail.com>
2015-11-19 21:34:53 +0000

    Cleaned up Tiphares plant block.

--- src/main/java/villain/mc/vague/blocks/BlockTipharesPlant.java
+++ src/main/java/villain/mc/vague/blocks/BlockTipharesPlant.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.ITileEntityProvider;
-import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
